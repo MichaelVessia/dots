ec0d318c35ec3beaf3c2b9aa1095c500a8d65dba
michaelvessia <michael@vessia.net>
2025-09-11 09:41:22 -0400

    try rose pine

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,14 @@
 return {
   -- Add colorscheme
-  { "ellisonleao/gruvbox.nvim", priority = 1000 },
+  --{ "ellisonleao/gruvbox.nvim", priority = 1000 },
   --{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  --{ "rose-pine/neovim", name = "rose-pine" },
+  { "rose-pine/neovim", name = "rose-pine", priority = 1000 },
 
   -- Configure LazyVim to load colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "rose-pine",
     },
   },
 }
