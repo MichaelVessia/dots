928368bb6d2b70492f60e725f1f272e07cdfd101
michaelvessia <michael@vessia.net>
2025-09-17 10:18:46 -0400

    meow?

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,11 @@
 return {
   -- Add colorscheme
-  --{ "ellisonleao/gruvbox.nvim", priority = 1000 },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  --{ "rose-pine/neovim", name = "rose-pine", priority = 1000 },
 
   -- Configure LazyVim to load colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      --colorscheme = "rose-pine",
       colorscheme = "catppuccin",
     },
   },
